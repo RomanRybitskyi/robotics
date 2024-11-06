1f8ccdc46f67197ca71f80e43e3e62f7e0b9c1bf
Roman <roman.3.ry@gmail.com>
2024-11-06 23:44:26 +0200

    Adding visualization in RViz and simulation in Gazebo

---
+++ ws/src/ground_drone_description/launch/display.launch.py
@@ -0,0 +1,61 @@
+# Filename: launch/ground_drone_launch.py
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Set the path to the robot description file
+    urdf_file = os.path.join(
+        get_package_share_directory('ground_drone_description'),
+        'urdf',
+        'ground_drone.urdf.xacro'
+    )
+    
+    # Set the path to Gazebo's launch file
+    gazebo_launch_file = os.path.join(
+        get_package_share_directory('gazebo_ros'),
+        'launch',
+        'gazebo.launch.py'
+    )
+    
+    # Declare Launch Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # Load robot state publisher to publish the URDF
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': Command(['xacro ', urdf_file]),
+            'use_sim_time': use_sim_time
+        }]
+    )
+    
+    # Start Gazebo with an empty world
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gazebo_launch_file),
+        launch_arguments={'verbose': 'true'}.items(),
+    )
+    
+    # Launch teleop_twist_keyboard node (optional)
+    teleop_node = Node(
+        package='teleop_twist_keyboard',
+        executable='teleop_twist_keyboard',
+        name='teleop_twist_keyboard',
+        output='screen',
+        prefix='xterm -e'  # Opens in a new terminal for ease of control
+    )
+    
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='true', description='Use simulation time if true'),
+        gazebo,
+        robot_state_publisher_node,
+        teleop_node
+    ])
+
